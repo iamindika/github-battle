722428feccd900ddfcad71ce997e09f9cefe2bb3
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-27 22:14:25 +0000

    Implement fetchPopularRepos function
    fetches repos based on the language parameter supplied
    imported function into Popular Component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { fetchPopularRepos } from '../utils/api';
 
 function LanguagesNav({selected, onUpdateLanguage}){
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
