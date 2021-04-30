ed67ebe0b399c9ea85584440a0fc3ba37bc4fce5
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-30 18:18:07 +0000

    Implement PlayerPreview Component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa';
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 
 function Instructions () {
@@ -87,7 +87,25 @@ PlayerInput.propTypes ={
 function PlayerPreview({username, onReset, label}) {
   return (
     <div className='column player' >
-      {username}
+      <h3 className='player-label'>{label}</h3>
+      <div className='row bg-light'>
+        <div className='player-info'>
+          <img
+            className='avatar-small'
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`} 
+          />
+          <a
+            href={`https://github.com/${username}`}
+            className='link' 
+          >
+            {username}
+          </a>
+        </div>
+        <button className='btn-clear flex-center' onClick={onReset}>
+          <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+        </button>
+      </div>
     </div>
   )
 }
