e5155494c8bc349a5f43116f2a2b5b49cf7998de
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-05-01 21:27:22 +0000

    Add Icon and user stats to Results component

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { battle } from '../utils/api';
+import {FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser} from 'react-icons/fa';
 
 export default class Results extends React.Component {
   constructor(props) {
@@ -57,6 +58,37 @@ export default class Results extends React.Component {
             src={winner.profile.avatar_url}
             alt={`Avatar for ${winner.profile.login}`} 
           />
+          <h2 className='center-text'>
+            <a className='link' href={winner.profile.html_url}>
+              {winner.profile.login}
+            </a>
+          </h2>
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {winner.profile.name}
+            </li>
+            {winner.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {winner.profile.location}
+              </li>
+            )}
+            {winner.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {winner.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {winner.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {winner.profile.following.toLocaleString()} following
+            </li>
+          </ul>
         </div>
         <div className='card bg-light'>
           <h4 className='header-lg center-text'>
@@ -67,6 +99,37 @@ export default class Results extends React.Component {
             src={loser.profile.avatar_url}
             alt={`Avatar for ${loser.profile.login}`} 
           />
+          <h2 className='center-text'>
+            <a className='link' href={loser.profile.html_url}>
+              {loser.profile.login}
+            </a>
+          </h2>
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {loser.profile.name}
+            </li>
+            {loser.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {loser.profile.location}
+              </li>
+            )}
+            {loser.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {loser.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {loser.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {loser.profile.following.toLocaleString()} following
+            </li>
+          </ul>
         </div>
       </div>
     )
