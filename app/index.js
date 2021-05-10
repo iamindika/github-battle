f071d61cf7f61fabd0127302713465e58d07c2cf
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-05-10 08:42:02 +0000

    Utilize ThemeProvider to pass down theme and toggleTheme from state to App component

--- app/index.js
+++ app/index.js
@@ -3,13 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Popular from './components/Popular';
 import Battle from './components/Battle';
+import { ThemeProvider } from './contexts/theme.js';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }));
+      }
+    }
+  }
+
   render() {
     return (
-      <div className='container'>
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className='container'>
+          <Battle />
+        </div>
+      </ThemeProvider>
+      
     );
   }
 }
